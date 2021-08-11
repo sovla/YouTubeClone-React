af97ce03be989ecc00d3542060df15c7def26932
sovla <41351496+sovla@users.noreply.github.com>
2021-08-11 12:56:39 +0900

    #2.0 complete(VideoList,detail,Search)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
+import _ from "lodash";
 
 import SearchBar from "./components/search_bar";
 import VideosList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 
-const YOUTUBE_API_KEY = "AIzaSyDVoniUhVllCwAifzKVxr1-BQSM6RLDvrU";
-
-YTSearch({ key: YOUTUBE_API_KEY, term: "surfboards" }, (viedos) => {
-  this.state({ viedos });
-});
+const YOUTUBE_API_KEY = "AIzaSyBz-NiHLXZyTowv3zmUwL41GP0lQxd8JMY";
+//AIzaSyBz-NiHLXZyTowv3zmUwL41GP0lQxd8JMY
+//AIzaSyDVoniUhVllCwAifzKVxr1-BQSM6RLDvrU
 
 class App extends Component {
   constructor(props) {
@@ -17,15 +17,32 @@ class App extends Component {
 
     //state 초깃값 설정
     this.state = {
-      viedos: [],
+      videos: [],
+      selectedVideo: null,
     };
+    this.videoSearch("surfboards");
+  }
+
+  videoSearch(term) {
+    console.log("API", term);
+
+    YTSearch({ key: YOUTUBE_API_KEY, term }, (videos) => {
+      this.setState({ videos: videos, selectedVideo: videos[0] });
+    });
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 1000);
     return (
       <div>
-        <SearchBar />
-        <VideosList viedos={this.state.viedos} />
+        <SearchBar onSearhTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideosList
+          videos={this.state.videos}
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+        />
       </div>
     );
   }
